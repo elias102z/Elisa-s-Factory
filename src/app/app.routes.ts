3e125614d92972a3b6ef2119038fad9aa75bc517
Stefan Kitzmüller <stefan.kitzmueller@gmx.net>
2025-03-11 15:24:56 +0100

    finished gallery including the tag function to get specific images

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { GalleryComponent } from './gallery/gallery.component';
@@ -6,9 +7,15 @@ import { ContactComponent } from './contact/contact.component';
 import { BookingFormComponent } from './booking-form/booking-form.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
-    {path:'gallery', component: GalleryComponent},
-    {path: 'services', component: BookingFormComponent},
-    {path: 'contact', component: ContactComponent}
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'services', component: BookingFormComponent },
+  { path: 'contact', component: ContactComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
