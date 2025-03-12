28c69ed5de4285f5798eff34b6ce5b0f15f996fa
Freddyro88 <freddyrosasdavila@gmail.com>
2025-03-12 12:17:39 +0100

    footer3

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const button = this.el.nativeElement.querySelector('.scroll-to-top');
+    if (window.scrollY > 300) {
+      this.renderer.addClass(button, 'visible');
+    } else {
+      this.renderer.removeClass(button, 'visible');
+    }
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
